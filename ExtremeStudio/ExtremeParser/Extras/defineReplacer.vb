746aadf08978918e7fc335e0a2652edf39337db0
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2015-10-17 22:56:25 +0300

    Fixed a bug with defineReplacer to make it not replace all #'s.

--- ExtremeStudio/ExtremeParser/Extras/defineReplacer.vb
+++ ExtremeStudio/ExtremeParser/Extras/defineReplacer.vb
@@ -9,18 +9,21 @@ Public Class defineReplacer
         findRegex = Regex.Replace(findRegex, "%([1-9])", "(.*)")
 
         'Find all. (We don't use Regex.Matches here cuz the codes are changed.)
-        While (Regex.IsMatch(Code, "\s" + findRegex + "\s"))
-            Dim Mtch As Match = Regex.Match(Code, findRegex)
+        findRegex = "(?<!#.*)\b(" + findRegex + ")\b"
+        While (Regex.IsMatch(Code, findRegex, RegexOptions.Multiline))
+            Dim Mtch As Match = Regex.Match(Code, findRegex, RegexOptions.Multiline)
 
             'Now build the replace code.
             Dim tmpDefineReplace As String = defineReplace
-            For i As Integer = 1 To Mtch.Groups.Count - 1
-                tmpDefineReplace = tmpDefineReplace.Replace("%" + i.ToString, Mtch.Groups(i).Value)
-            Next
+            If tmpDefineReplace.Contains("%") Then
+                For i As Integer = 1 To Mtch.Groups.Count - 1
+                    tmpDefineReplace = tmpDefineReplace.Replace("%" + i.ToString, Mtch.Groups(i + 1).Value)
+                Next
+            End If
 
             'Now replace actually.
-            Code = Code.Remove(Mtch.Index, Mtch.Length)
-            Code = Code.Insert(Mtch.Index, tmpDefineReplace)
+            Code = Code.Remove(Mtch.Groups(1).Index, Mtch.Groups(1).Length)
+            Code = Code.Insert(Mtch.Groups(1).Index, tmpDefineReplace)
         End While
     End Sub
 End Class
