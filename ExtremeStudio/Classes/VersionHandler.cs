d964ba4edb047ecfaa2f2416348a3464830786d8
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2018-05-31 03:40:47 +0200

    added support to convert old version projects

--- ExtremeStudio/Classes/VersionHandler.cs
+++ ExtremeStudio/Classes/VersionHandler.cs
@@ -1,11 +1,44 @@
-﻿namespace ExtremeStudio.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ExtremeCore.Classes;
+
+namespace ExtremeStudio.Classes
 {
     public class VersionHandler
     {
-        public string CurrentVersion {get; set;} = "1.0.0";
+        public string CurrentVersion {get;} = "2.0.0";
 	
         public void DoIfUpdateNeeded(CurrentProjectClass project)
         {
+            if (project.ProjectVersion == "1.0.0")
+            {
+                if (!Directory.Exists(project.ProjectPath + "/dependencies"))
+                {
+                    //Setup sampctl for it
+                    project.SampCtlData = new PawnJson()
+                    {
+                        entry = "gamemodes\\" + project.ProjectName + ".pwn",
+                        output = "gamemodes\\" + project.ProjectName + ".amx",
+                        user = Environment.UserName,
+                        repo = project.ProjectName,
+                        dependencies = new List<string>() {"sampctl/samp-stdlib"},
+                        builds = new List<BuildInfo>()
+                        {
+                            new BuildInfo()
+                            {
+                                name = "main",
+                                args = Program.SettingsForm.GetCompilerArgs().Split(' ').ToList()
+                            }
+                        },
+                        runtime = new RuntimeInfo() {version = "latest"},
+                    };
+                    project.SaveInfo();
+                    SampCtl.SendCommand(Path.Combine(Application.StartupPath, "sampctl.exe"), project.ProjectPath, "p ensure");
+                }
+            }
             project.ProjectVersion = CurrentVersion;
         }
     }
